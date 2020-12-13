a77e12087e0b77b551a3bc0fc79e6712215c9e44
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-12-13 10:51:34 +0200

    new file:   .gitignore new file:   package-lock.json new file:   package.json new file:   src/about/assets/img/soldier.jpg new file:   src/about/assets/img/thumb.jpg new file:   src/about/assets/img/triumph.jpg new file:   src/about/assets/js/index.js new file:   src/about/assets/scss/style.scss new file:   src/about/index.html new file:   src/main/assets/img/bike-super-duke-ktm.jpg new file:   src/main/assets/img/intel-core-i7.jpg new file:   src/main/assets/img/yamaha-r6-bike.jpg new file:   src/main/assets/js/index.js new file:   src/main/assets/scss/style.scss new file:   src/main/index.html new file:   webpack.config/about.webpack.config.js new file:   webpack.config/main.webpack.config.js new file:   workflow.txt

---
+++ webpack.config/about.webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/about/assets/js/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist/about'),
+    filename: 'assets/js/bundle.js',
+    publicPath: ''
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist/about'),
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ['html-loader']
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../',
+            },
+          }, 
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/img/[contenthash].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/fonts/[contenthash].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/about/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/style.css'
+    })
+  ]
+};
